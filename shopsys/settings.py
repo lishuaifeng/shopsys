af1513e4d41453342cf5b3cd4b03d3f95bc8af18
lishuaifeng <chengfengpolang@aliyun.com>
2017-04-27 16:05:59 +0800

    配置mysql数据库

--- shopsys/settings.py
+++ shopsys/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'shopsys.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'shopsys_db',
+        'USER':'root',
+        'PASSWORD':'root',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
     }
 }
 
@@ -105,7 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+DATE_FORMAT='Y-m-d'
+
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
